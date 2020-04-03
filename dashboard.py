570b6909e20cad709f1a2c1ec53c100694638618
Val <v.albrecht@kayrros.com>
2020-04-03 10:47:22 +0200

    3D map with pydeck.

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,8 @@ import pandas as pd
 import streamlit as st
 from datetime import date, timedelta
 from plotly import graph_objs as go
+import pydeck
+from resources import utils
 
 @st.cache
 def get_global_data(last_date=date.today() - timedelta(1), ts_type='Confirmed'):
@@ -35,7 +37,6 @@ def get_detailed_daily_reports(last_date=date.today() - timedelta(1), ts_type='C
     df = pd.DataFrame(index=pd.date_range(start='2020-01-22', end=last_date))
     states = pd.read_pickle('resources/states.pkl')
     a2_to_fullname = {x['abbr']: x['name'] for x in states}
-
 
     for day in pd.date_range(start='2020-01-22', end=last_date):
         daily_link = f'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{day.strftime("%m-%d-%Y")}.csv'
@@ -110,19 +111,94 @@ def _max_width_(nb_pixels=1500):
         unsafe_allow_html=True,
     )
 
+
+@st.cache(persist=True)
+def get_world_as_polygons():
+    """Can't get GeoJson Layers to work on pydeck, nor multipolygon, so we'll hack it with
+    Polygons and redondant countries
+    """
+    raw = pd.read_json('resources/countries.geojson')
+    df = pd.DataFrame()
+
+    df['coords'] = raw.features.apply(lambda x: x['geometry']['coordinates'])
+    df['country'] = raw.features.apply(lambda x: x['properties']['ADMIN'])
+    df['len_poly'] = df.coords.apply(lambda x: len(x))
+
+    simple = df.query('len_poly==1')
+    complicated = df.query('len_poly>1')
+    complicated = complicated.explode('coords')
+
+    return simple.append(complicated)[['coords', 'country']]
+
+def _hide_menu_():
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+    """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 def main():
     _max_width_()
+    _hide_menu_()
     st.title('Stay the fuck Home :derelict_house_building:')
-    df_type = st.sidebar.selectbox('World Global or US States', ['Global', 'US States'])
     ts_type = st.sidebar.selectbox('Confirmed Cases / Deaths', ['Confirmed', 'Deaths'])
-    df = get_global_data(ts_type=ts_type) if df_type=='Global' else get_detailed_daily_reports(ts_type=ts_type)
 
-    default_countries = ['France', 'Italy', 'Spain', 'US'] if df_type == 'Global' else list(df.columns.values)
+    show_graph = st.checkbox('Show Graph ?', False)
+    if show_graph:
+        df_type = st.sidebar.selectbox('World Global or US States', ['Global', 'US States'])
+        df = get_global_data(ts_type=ts_type) if df_type=='Global' else get_detailed_daily_reports(ts_type=ts_type)
+        default_countries = ['France', 'Italy', 'Spain', 'US'] if df_type == 'Global' else list(df.columns.values)
+        highlighted_cols = st.multiselect('Select multiple countries, others will be hidden on chart.', list(df.columns.values), default_countries)
 
-    highlighted_cols = st.multiselect('Select multiple countries, others will be hidden on chart.', list(df.columns.values), default_countries)
+        st.plotly_chart(plot_df(df, highlighted_cols, plot_title=df_type,))
 
-    st.plotly_chart(plot_df(df, highlighted_cols, plot_title=df_type,))
-    st.info('_Data fetched from https://github.com/CSSEGISandData/COVID-19_')
+    show_map = st.checkbox('Show Map?', True)
+    if show_map:
+        d = st.date_input('Choose date', date.today() - timedelta(1))
+
+        df_to_join = get_global_data(ts_type=ts_type).loc[d]
+        polygons = get_world_as_polygons()
+        df_to_join = df_to_join.rename(index=utils.COUNTRIES_NAME)
+        display = polygons.join(df_to_join, on='country').dropna()
+        display.columns = ['coords', 'country', 'cases']
+        display['color'] = pd.cut(display.cases,
+            bins=len(utils.COLOR_RANGE),
+            labels=[str(x) for x in utils.COLOR_RANGE],
+            include_lowest=True)
+
+        geojson = pydeck.Layer(
+            'PolygonLayer',
+            display,
+            opacity=1,
+            get_polygon='coords',
+            stroked=True,
+            filled=True,
+            extruded=True,
+            # wireframe=True,
+            get_elevation='cases * 10',
+            elevation_range=[0, 100000],
+            get_fill_color='color',
+            get_line_color='color',
+            pickable=True
+        )
+
+        INITIAL_VIEW_STATE = pydeck.ViewState(
+            latitude=0,
+            longitude=0,
+            zoom=1,
+            max_zoom=8,
+            pitch=25,
+            bearing=0
+        )
+
+        st.pydeck_chart(pydeck.Deck(
+            layers=[geojson],
+            initial_view_state=INITIAL_VIEW_STATE))
+
+
+    st.info('_Covid Data fetched from https://github.com/CSSEGISandData/COVID-19_')
+    st.info('_Countries geojson downloaded at https://datahub.io/core/geo-countries_')
     st.info('_Code available at https://github.com/PapaEcureuil/covid_dashboard_')
 
 if __name__ == "__main__":
---
+++ resources/utils.py
@@ -0,0 +1,37 @@
+# Countries name not consistent between COVID dataset and Geojson data
+COUNTRIES_NAME = {
+    'Bahamas': 'The Bahamas',
+    'Burma': 'Myanmar',
+    'Cabo Verde': 'Cape Verde',
+    'Congo (Brazzaville)': 'Republic of Congo',
+    'Congo (Kinshasa)': 'Democratic Republic of the Congo',
+    'Cote d\'Ivoire': 'Ivory Coast',
+    'Czechia': 'Czech Republic',
+    'Eswatini': 'Swaziland',
+    'Guinea-Bissau': 'Guinea Bissau',
+    'Holy See': 'Vatican',
+    'Korea, South': 'South Korea',
+    'North Macedonia': 'Macedonia',
+    'Serbia': 'Republic of Serbia',
+    'Taiwan*': 'Taiwan',
+    'Tanzania': 'United Republic of Tanzania',
+    'Timor-Leste': 'East Timor',
+    'US': 'United States of America',
+    'West Bank and Gaza' : 'Palestine'
+}
+
+COLOR_RANGE = [
+    [65, 182, 196],
+    [127, 205, 187],
+    [199, 233, 180],
+    [237, 248, 177],
+    [255, 255, 204],
+    [255, 237, 160],
+    [254, 217, 118],
+    [254, 178, 76],
+    [253, 141, 60],
+    [252, 78, 42],
+    [227, 26, 28],
+    [189, 0, 38],
+    [128, 0, 38],
+]
